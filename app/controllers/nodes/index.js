ada11cce1d92b5cddd437e077501c735f7c9623e
Charles Williams <cwilli34@nd.edu>
2016-05-27 18:25:11 -0400

    Added the ability to delete records, created confirmation modal and created widget directory.

--- app/controllers/nodes/index.js
+++ app/controllers/nodes/index.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    responseMessage: '',
+    responseSuccess: '',
+    responseError: '',
+    selectedModel: '',
     actions: {
         createNode: function(title, description) {
             var node = this.store.createRecord('node', {
@@ -9,24 +11,56 @@ export default Ember.Controller.extend({
                 category: 'project',
                 description: description || null
             });
+
             var post = this.get('model');
             var self = this;
 
             // TODO: Fix this
-            this.set('responseMessage', `Your project was created successfully!`);
+
             node.save().then(function() {
-                console.log(title + ' ' + description);
+                self.set('responseError', '');
+                self.set('responseSuccess', `Your project was created successfully!`);
+                self.set('title', '');
+                self.set('description', '');
+                post.reload();
+            }, function(error) {
+                console.log(error.message);
+                self.set('responseSuccess', '');
+                self.set('responseError', 'There was an error creating your project.');
                 self.set('title', '');
                 self.set('description', '');
                 post.reload();
             });
-            console.log(self);
-            console.log(title + ' ' + description);
+        },
+        requestDelete: function(node, name) {
+            console.log(node);
+            this.set('selectedModel', node);
+            this.selectedModel.deleteRecord();
+            $('.ui.modal').modal('show');
 
         },
+        confirmDelete: function() {
+            console.log(this.selectedModel);
+            var page = this.get('model');
+
+            this.selectedModel.save().then(function() {
+                page.reload();
+            });
+        },
+        cancelDelete: function() {
+            console.log(this.selectedModel);
+            this.selectedModel.rollbackAttributes();
+        },
+        destroyNode: function(node) {
+            var page = this.get('model');
+
+            node.destroyRecord().then(function() {
+                page.reload();
+            });
+        },
         closeMessage: function() {
-            console.log('Close!');
-            this.set('responseMessage', '');
+            this.set('responseSuccess', '');
+            this.set('responseError', '');
         }
     }
 });
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -29,31 +29,6 @@ module.exports = function (defaults) {
         },
     });
 
-    /*app.import({
-        development: 'bower_components/semantic-ui/dist/semantic.css',
-        production: 'bower_components/semantic-ui/dist/semantic.min.css'
-    });*/
-
-    /*app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
-    app.import('bower_components/bootstrap/dist/css/bootstrap-theme.min.css');
-    app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
-    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
-        destDir: 'fonts'
-    });
-    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', {
-        destDir: 'fonts'
-    });
-    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg', {
-        destDir: 'fonts'
-    });
-    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', {
-        destDir: 'fonts'
-    });
-    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2', {
-        destDir: 'fonts'
-    });*/
-
-
     // Use `app.import` to add additional libraries to the generated
     // output files.
     //
