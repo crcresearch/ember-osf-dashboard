bdec4ff63106dab87fc88f1d527231b3cc2ff122
Charles Williams <cwilli34@nd.edu>
2016-06-23 14:16:57 -0400

    TODO added

--- app/pods/components/pagination-control-ui/component.js
+++ app/pods/components/pagination-control-ui/component.js
@@ -4,5 +4,6 @@ import PaginationControl from 'ember-osf/components/pagination-control/component
 import layout from './template';
 
 export default PaginationControl.extend({
+    // TODO:0:ember-issue:11678
     layout
 });
